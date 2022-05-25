68f41c8050f418ded6834d2ba788a4460017e351
CiocanGeorge <george.ciocan@crmsoftware.ro>
2022-05-25 13:11:13 +0300

    create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css';
+//component
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Ed's Todo List</h1>
+      </header>
+      <Form/>
     </div>
   );
 }
